d248ad620832bfca944469c6798d7025ebb3ab58
Connor Mort <rc0029805@student.riversidecollege.ac.uk>
2019-06-04 10:41:27 +0100

    assignment2

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            double R1, R2, resistance;
+            R1 = Convert.ToDouble(textBox1.Text);
+            R2 = Convert.ToDouble(textBox2.Text);
+            resistance = R1 + R2;
+            label7.Text = "resistance = " + resistance;
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
